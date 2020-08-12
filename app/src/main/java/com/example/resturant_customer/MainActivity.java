62cb8e4c8d19fbd02bc21c63f2bf06420134b907
Panth112802 <36990771+Panth112802@users.noreply.github.com>
2020-08-12 09:52:42 +0200

    firsat

---
+++ app/src/main/java/com/example/resturant_customer/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.resturant_customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import java.util.zip.Inflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+       inflater.inflate(R.menu.menu_res,menu);
+        return true;
+    }
+}
